350646905089edf39fd0f39f8eb13c2d83938202
Mikhail Turilov <turilovm@gmail.com>
2022-12-19 21:38:58 +0300

    add tests

---
+++ src/test/java/Tests/TestDemoQa.java
@@ -0,0 +1,71 @@
+package Tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+
+public class TestDemoQa {
+    @BeforeAll
+    static void setUp(){
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void formTest(){
+        step("Open registrations form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+        });
+        step("Fill form", () -> {
+        $("[id=firstName]").setValue("Mike");
+        $("[id=lastName]").setValue("Turilov");
+        $("[id=userEmail]").setValue("example@google.com");
+        $("[id=gender-radio-1]").doubleClick();
+        $("[id=userNumber]").setValue("9001546995");
+        $("[id=dateOfBirthInput]").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1985");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__day--012").click();
+        $("#subjectsInput").click();
+        $("#subjectsInput").setValue("Maths");
+        $("#subjectsInput").pressEnter();
+        $("#subjectsInput").pressTab();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/1.png"));
+        $("#currentAddress").setValue("Some Address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        });
+
+        step("Check form results", () -> {
+        $(".modal-content").shouldBe(Condition.visible);
+        $(".modal-content").shouldHave(text("Mike Turilov"));
+        $(".modal-content").shouldHave(text("example@google.com"));
+        $(".modal-content").shouldHave(text("Male"));
+        $(".modal-content").shouldHave(text("9001546995"));
+        $(".modal-content").shouldHave(text("12 January,1985"));
+        $(".modal-content").shouldHave(text("Maths"));
+        $(".modal-content").shouldHave(text("Sports"));
+        $(".modal-content").shouldHave(text("1.png"));
+        $(".modal-content").shouldHave(text("Some Address"));
+        $(".modal-content").shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
+        });
+
+    }
+}
