2f96096f57572594412f365be7b239fbc5cf9fda
Mikhail Turilov <turilovm@gmail.com>
2022-12-21 22:01:22 +0300

    add tests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,17 +23,17 @@ public class TestBase {
 //        capabilities.setCapability("enableVideo", true);
 //        Configuration.browserCapabilities = capabilities;
     }
-
-    @BeforeEach
-    void addListener() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-    }
+//
+//    @BeforeEach
+//    void addListener() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//    }
+//
+//    @AfterEach
+//    void addAttachments() {
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+//        Attach.browserConsoleLogs();
+//        Attach.addVideo();
+//    }
 }
